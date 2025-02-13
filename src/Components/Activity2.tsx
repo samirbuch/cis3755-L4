f2a82e63eabe2107057d744fb85afe0e200fb183
bdotsamir <samir.js.zip@gmail.com>
2025-02-13 18:07:05 -0500

    Activity2: create axis

--- src/Components/Activity2.tsx
+++ src/Components/Activity2.tsx
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useMemo, useRef } from "react";
 import { DefaultProps } from "../types/DefaultProps";
 import { Car } from "../types/Car";
 
@@ -10,22 +10,32 @@ export default function Activity2(props: Activity2Props) {
 
   const d3Ref = useRef<SVGSVGElement>(null);
 
+  const scale = useMemo(() => d3.scaleLinear(), []);
+  const axis = useMemo(() => d3.axisBottom(scale), [scale]);
+
   function createVis() {
+    if (!d3Ref.current) return;
+
     d3.select(d3Ref.current)
       .selectAll("circle")
       .data(props.data)
       .join(
-        function(enter) {
+        function (enter) {
           return enter.append("circle").style("opacity", 0.25)
         },
-        function(update) {
+        function (update) {
           return update.style("opacity", 1)
         }
       )
-      .attr("cx", (d, i) => i * 10)
+      .attr("cx", (d, i) => scale(i * 10))
       .attr("cy", 50)
       .attr("r", (d: Car) => d.hwy * 0.5)
-      .style("fill", "blue")
+      .style("fill", "blue");
+
+    d3.select(d3Ref.current)
+      .append("g")
+      .transition()
+      .call(axis).attr("transform", "translate(0,90)");
   }
 
   useLayoutEffect(() => {
@@ -36,8 +46,8 @@ export default function Activity2(props: Activity2Props) {
 
   return (
     <div style={{ height: props.height, width: props.width }}>
-      <svg ref={d3Ref} height={props.height} width={props.width} /> 
-      {/* <button onClick={() => createVis()}>Click me!</button> */}
+      <svg ref={d3Ref} height={props.height} width={props.width} />
+      <button onClick={() => createVis()}>Click me!</button>
     </div>
   )
 }
